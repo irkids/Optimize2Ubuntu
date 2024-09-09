f086cb3771564070aa9fbe9fdd5ac33c88fb49c3
irkids <147476857+irkids@users.noreply.github.com>
2024-09-09 11:54:19 +0330

    Update Optimize2Ubuntu.sh

--- Optimize2Ubuntu.sh
+++ Optimize2Ubuntu.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-sudo apt-get update -y && apt-get upgrade -y
+sudo apt-get update -y && sudo apt-get dist-upgrade -y
 sudo apt-get install pkg-config libssl-dev -y
 apt install curl socat -y
+sudo apt-get install jq -y
+sudo apt install openssh-server -y
 sudo service ssh restart
 sudo apt-get -y install software-properties-common apt-get install -y stunnel4 cmake screenfetch openssl
 sudo add-apt-repository ppa:ondrej/php -y
+sudo apt-get -y install software-properties-common
 apt-get install apache2 zip unzip net-tools curl mariadb-server -y
 apt-get install php php-cli php-mbstring php-dom php-pdo php-mysql -y
 apt-get install npm -y
@@ -13,12 +16,13 @@ sudo apt-get install coreutils
 apt install php8.1 php8.1-mysql php8.1-xml php8.1-curl cron -y
 apt install php8.2 php8.2-mysql php8.2-xml php8.2-curl cron -y
 apt install php8.3 php8.3-mysql php8.3-xml php8.3-curl cron -y
+sudo apt-get install php8. php-pear -y
+sudo apt install php8.1 php8.1-fpm php8.1-mysql php8.1-curl php8.1-gd php8.1-mbstring php8.1-xml php8.1-xmlrpc php8.1-zip -y
 sudo apt-get install git -y
+sudo apt install iftop -y
 sudo apt install perl -y
-sudo apt install libdbd-mysql-perl -y
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install Python3.12 -y
 sudo apt install python -y
 sudo apt-get install nano
 sudo apt-get install ufw
