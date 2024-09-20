66d758526f79aadca99946e31cb0ed147c63bee0
irkids <147476857+irkids@users.noreply.github.com>
2024-09-20 23:03:51 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,14 @@ set -e
 sudo sh -c 'echo "deb http://archive.ubuntu.com/ubuntu focal main universe" >> /etc/apt/sources.list'
 
 # Update the package list
-sudo apt update
+sudo apt-get update
 
 # Upgrade installed packages
 sudo apt-get dist-upgrade -y
+
+# Change DNS server to Google DNS
+sudo sed -i 's/nameserver.*/nameserver 8.8.8.8\nnameserver 8.8.4.4/' /etc/resolvconf
+sudo resolvconf -u
 
 # Install necessary packages for productivity and optimization
 # Group 1: Networking and monitoring tools
